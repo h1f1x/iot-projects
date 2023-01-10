4264a0122ff0556d5b084aa7967dd032886a5036
The RPI <pi@fixrpi2.local>
2023-01-10 22:17:18 +0100

    add sms

---
+++ sim2070iot/sms.py
@@ -0,0 +1,15 @@
+from sim2070iot import SIM2070
+
+import click
+
+@click.command()
+@click.option('--msg', prompt='Nachricht')
+@click.option('--number', prompt='telefonnummer',
+              help='SMS Ziel.')
+def hello(msg, number):
+    sim = SIM2070()
+    sim.sendSMS(number=number, text=msg)
+
+
+if __name__ == '__main__':
+    hello()
